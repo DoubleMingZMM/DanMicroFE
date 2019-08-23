396ff3f5c9c33bfa6155d18c74bccfa379efb5d5
Daniel <1602268076@qq.com>
2019-08-23 14:30:43 +0800

    fix react app

--- react/src/root.component.js
+++ react/src/root.component.js
@@ -4,12 +4,12 @@
  * @Author: Daniel
  * @Date: 2019-08-23 13:25:05
  * @LastEditors: Daniel
- * @LastEditTime: 2019-08-23 13:26:57
+ * @LastEditTime: 2019-08-23 14:23:45
  */
 import React from 'react';
 
 export default class Root extends React.Component {
     render() {
-        return ret = <div>This was rendered by react.</div>;
+        return (<div>This was rendered by react.</div>);
     }
 }
